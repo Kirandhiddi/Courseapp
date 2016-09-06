7765c64703a19e710fa0d45dd45d05a2b6871ccb
[kiran] <[kiran.dhiddi1212@gmail.com]>
2016-09-06 11:09:46 -0500

    third commit

--- courseapp/src/main/java/com/courseapp/domain/Course.java
+++ courseapp/src/main/java/com/courseapp/domain/Course.java
@@ -2,10 +2,8 @@ package com.courseapp.domain;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-//import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
